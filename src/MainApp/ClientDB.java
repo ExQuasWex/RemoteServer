9cb7c34aee65bdd69efc0e5815e97d5ec677d38c
ExQuasWex <villerdex@Gmail.com>
2015-10-04 20:53:01 +0800

    client Window

--- src/MainApp/ClientDB.java
+++ src/MainApp/ClientDB.java
@@ -77,7 +77,7 @@ public class ClientDB extends UnicastRemoteObject implements RemoteMethods  {
     }
 
 
-    //METHODS THAT ARE NEED TO BE SYNCRONIZED asdasd
+    //METHODS THAT ARE NEED TO BE SYNCRONIZED
 
     @Override
     public boolean Login(String user, String pass) throws RemoteException {
